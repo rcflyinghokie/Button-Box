1f7e7571418f9329456307c84b656cb92c357827
Ryan Callaway <rcflyinghokie@gmail.com>
2021-03-27 13:00:18 -0400

    Update On_On_Testing.ino

--- On_On_Testing.ino
+++ On_On_Testing.ino
@@ -8,22 +8,21 @@
 #include <Joystick.h>
 
 #define ENABLE_PULLUPS
-#define NUMBUTTONS 17
-#define NUMROWS 2
-#define NUMCOLS 6
+#define NUMBUTTONS 8
+#define NUMROWS 1
+#define NUMCOLS 4
 
 
 //Matrix defining button numbers as seen by Windows, 0 is button 1 and so forth, "99" is a "placeholder" position
 byte buttons[NUMROWS][NUMCOLS] = {
-  {0, 2, 3, 5, 6, 8},
-  {10, 12, 13, 15, 99, 99},
+  {0, 2, 4, 6},
 };
 
 // connect to the row pinouts of the keypad
-byte rowPins[NUMROWS] = {A3, A2};
+byte rowPins[NUMROWS] = {A3};
 
 // connect to the column pinouts of the keypad
-byte colPins[NUMCOLS] = {16, 10, 15, 14, 8, 9};
+byte colPins[NUMCOLS] = {A2, A1, A0, 15};
 
 Keypad buttbx = Keypad( makeKeymap(buttons), rowPins, colPins, NUMROWS, NUMCOLS);
 
@@ -35,8 +34,8 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
                    false, false,          // No rudder or throttle
                    false, false, false);  // No accelerator, brake, or steering
 
-int TwoPosSwitch[] = {6, 8, 13, 14}; // List of all 2 position switch buttons wired
-int ThreePosSwitch[] = {0, 2, 3, 5, 10, 12}; // List of all 3 position switch buttons wired
+//int TwoPosSwitch[] = {0, 2, 4, 6}; // List of all 2 position switch buttons wired
+//int ThreePosSwitch[] = {0, 2, 3, 5, 10, 12}; // List of all 3 position switch buttons wired
 
 void setup() {
   Joystick.begin();
@@ -44,6 +43,7 @@ void setup() {
 
 void loop() {
   CheckAllButtons();
+  //CheckTwoPos();
 }
 
 void CheckAllButtons(void) {
@@ -55,17 +55,14 @@ void CheckAllButtons(void) {
       {
         switch (buttbx.key[i].kstate) {
           case PRESSED:
+            Joystick.setButton(i + 1, 0);
           case HOLD:
             Joystick.setButton(buttbx.key[i].kchar, 1);
             break;
           case RELEASED:
+            Joystick.setButton(i + 1, 1);
           case IDLE:
             Joystick.setButton(buttbx.key[i].kchar, 0);
-  
-            if (TwoPosSwitch = i)
-            {
-              Joystick.setButton(buttbx.key[i + 1].kchar, 1);
-            }
             break;
         }
       }
